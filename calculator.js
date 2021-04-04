0c3620c8ed2cc954430a3b9b0cfa9f9abb708924
Awoyomi Mayowa <mayowaawoyomi@gmail.com>
2021-04-04 23:07:55 +0100

    the clear button now works, secondInput can take more than one value

--- calculator.js
+++ calculator.js
@@ -3,69 +3,112 @@ const keys = calculator.querySelector('.calculator__keys')
 const display = document.querySelector('.calculator__display')
 
 
-const calculate = (n1, operator, n2) => {
-    let result = ''
+const calculate = (firstInput, operator, secondInput) => {
+    let sum = ''
 
     if (operator === 'add') {
-        result = parseFloat(n1) + parseFloat(n2)
+        sum = parseFloat(firstInput) + parseFloat(secondInput)
     } else if (operator === 'subtract') {
-        result = parseFloat(n1) - parseFloat(n2)
+        sum = parseFloat(firstInput) - parseFloat(secondInput)
     } else if (operator === 'multiply') {
-        result = parseFloat(n1) * parseFloat(n2)
+        sum = parseFloat(firstInput) * parseFloat(secondInput)
     } else if (operator === 'divide') {
-        result = parseFloat(n1) / parseFloat(n2)
+        sum = parseFloat(firstInput) / parseFloat(secondInput)
     }
 
-    return result
+    return sum
 }
 
 keys.addEventListener('click', (e) => {
-    //it was a button i pressed
+    //if was a button i pressed
     if (e.target.matches('button')) {
         const keyPressed = e.target
         const action = keyPressed.dataset.action //if the button has a dataaction
         const keyPressedContent = keyPressed.textContent //the value of the key pressed
-        const displayedNum = display.textContent
+        const displayedNum = display.textContent //my screen display
         const previousKeyType = calculator.dataset.previousKeyType
 
-
-        Array.from(keyPressed.parentNode.children)
-            .forEach(k => k.classList.remove('operatorkey')
-            )
-
-        // the value displaying on the screen
+        // if it is a number digit i pressed
         if (!action) {
 
-            //if the number on the screen is zero then change it to the key pressed
-            //if it is not zero then add other number that are pressed
-            (displayedNum === '0' || previousKeyType === 'operator') ? display.textContent = keyPressedContent : display.textContent = displayedNum + keyPressedContent
+            (displayedNum === '0' || previousKeyType === 'operator' || previousKeyType === 'calculate') ? display.textContent = keyPressedContent : display.textContent = displayedNum + keyPressedContent
+            calculator.dataset.previousKeyType = 'number'
+        }
+
+
+        // if it is a decimal point
+        if (action === 'decimal') {
+
+            //if what is already on the screen is not a decimal point then add decimal to the existing value
+            if (!displayedNum.includes('.')) {
+                display.textContent = displayedNum + '.'
+            } else if (previousKeyType === 'operator' || previousKeyType === 'calculate') {
+                display.textContent = '0.' 
+            }
+
+            calculator.dataset.previousKeyType = 'decimal'
 
         }
 
-        if (action === 'decimal') display.textContent = displayedNum + '.'
+
         
-
-        // when i click any operator i want it to show the operator i clicked
         if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
-            keyPressed.classList.add('operatorkey')
-
-            console.log(keyPressed.parentNode.children)
-            
-            calculator.dataset.previousKeyType = 'operator'
-            calculator.dataset.firstValue = displayedNum
-            calculator.dataset.operator = action
-
-        }
-
-
-        if (action === 'clear') console.log('clear key!')
-          
-
-        if (action === 'calculate') {
             const firstValue = calculator.dataset.firstValue
             const operator = calculator.dataset.operator
             const secondValue = displayedNum
-            display.textContent = calculate(firstValue, operator, secondValue)
+
+            if (firstValue && operator && previousKeyType !== 'operator' && previousKeyType !== 'calculate') {
+                const calcValue = calculate(firstValue, operator, secondValue)
+                display.textContent = calcValue
+                calculator.dataset.firstValue = calcValue
+            } else {
+                calculator.dataset.firstValue = displayedNum
+            }
+
+            calculator.dataset.previousKeyType = 'operator'
+            calculator.dataset.operator = action
+
+
+        }
+
+
+        if (action === 'clear') {
+            console.log('clear key!')
+            if (keyPressed.textContent === 'AC') {
+                calculator.dataset.firstValue = ''
+                calculator.dataset.modValue = ''
+                calculator.dataset.operator = ''
+                calculator.dataset.previousKeyType = ''
+            } else {
+                keyPressed.textContent = 'AC'
+            }
+
+            display.textContent = 0
+            calculator.dataset.previousKeyType = 'clear'
+        }
+
+        if (action !== 'clear') {
+            const clearButton = calculator.querySelector('[data-action=clear]')
+            clearButton.textContent = 'CE'
+        }
+
+
+        if (action === 'calculate') {
+            let firstValue = calculator.dataset.firstValue
+            let operator = calculator.dataset.operator
+            let secondValue = displayedNum
+
+            if (firstValue) {
+                if (previousKeyType === 'calculate') {
+                    firstValue = displayedNum
+                    secondValue = calculator.dataset.modValue
+                }
+
+                display.textContent = calculate(firstValue, operator, secondValue)
+            }
+
+            calculator.dataset.modValue = secondValue
+            calculator.dataset.previousKeyType = 'calculate'
         }
 
 
